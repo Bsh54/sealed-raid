b8e40b4cae32aa0f45c1171094d90f9ab8bd600f
Bsh54 <shadrakbsh@gmail.com>
2026-08-06 11:41:44 +0100

    Set deployed SealedRaid address on Base Sepolia

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,7 +1,7 @@
 import { sealedRaidAbi } from "@/lib/abi/sealedRaid";
 
 export const SEALED_RAID_ADDRESS = (process.env.NEXT_PUBLIC_SEALED_RAID_ADDRESS ??
-  "0x0000000000000000000000000000000000000000") as `0x${string}`;
+  "0x7d3eb98211f0b7e8511ad161e598ad87ee2a7b04") as `0x${string}`;
 
 export const sealedRaidContract = {
   address: SEALED_RAID_ADDRESS,
